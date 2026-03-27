dbb6b6c7a8009241225249096eb8ea635e5996ba
vignesh1605 <a.vignesh1605@outlook.com>
2026-03-27 07:12:59 +0530

    Hooks implemented for get driver and driver close

--- AutomationPracticeProject/src/test/java/StepDefination/Hooks.java
+++ AutomationPracticeProject/src/test/java/StepDefination/Hooks.java
@@ -1,5 +1,25 @@
 package StepDefination;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import utils.TestContext;
+
 public class Hooks {
+
+    private TestContext testContext;
+
+    public Hooks(TestContext testContext) {
+        this.testContext = testContext;
+    }
+
+    @Before
+    public void setUp() {
+        testContext.initializeDriver();  // Driver starts here
+    }
+
+    @After
+    public void tearDown() {
+        testContext.quitDriver();  // Driver closes here
+    }
 
 }
